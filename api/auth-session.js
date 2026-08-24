ac283f69dd9204521016392f1bb97c7bdcda45b1
Darshan-ukey <ukey.darsh@gmail.com>
2026-08-24 11:29:44 +0530

    LTL + Spatial map V1

---
+++ api/auth-session.js
@@ -0,0 +1,1 @@
+'use strict';const {json}=require('./_utils');const {userFromReq}=require('./_auth');module.exports=async(req,res)=>{const user=await userFromReq(req);json(res,200,{ok:true,authenticated:Boolean(user),user:user||null})};
